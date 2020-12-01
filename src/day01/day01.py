f03f07d9dd9085daffc69b214507dd63247cffd6
Jose Manuel Llorens <jose.llorens.ripolles@gmail.com>
2020-12-01 19:15:34 +0100

    Day01

---
+++ src/day01/day01.py
@@ -0,0 +1,26 @@
+def part01():
+    for i in range(0, len(lines)):
+        for j in range(i, len(lines)):
+            if lines[i] + lines[j] == 2020:
+                print("Part1 solution: {solution}".format(
+                    solution=lines[i]*lines[j]))
+                return
+
+
+def part02():
+    for i in range(0, len(lines)):
+        for j in range(i, len(lines)):
+            for k in range(j, len(lines)):
+                if lines[i] + lines[j] + lines[k] == 2020:
+                    print("Part2 solution: {solution}".format(
+                        solution=lines[i]*lines[j]*lines[k]))
+                    return
+
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        lines = f.read().splitlines()
+
+    lines = list(map(int, lines))
+    part01()
+    part02()
